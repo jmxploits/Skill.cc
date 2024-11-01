c5f6341e5ce9109089e318220046a26a8f9d30f1
jmxploits <121002036+jmxploits@users.noreply.github.com>
2024-11-01 16:38:27 +0800

    Create ValidHWID.lua

---
+++ ValidHWID.lua
@@ -0,0 +1,19 @@
+-- // FOR YENO
+-- example of how to do this
+-- ask people to execute this inside any game
+-- setclipboard(game:GetService("RbxAnalyticsService"):GetClientId())
+-- then ask them to tell you what was pasted inside
+-- put in this format
+-- return {
+--  "ExampleBuyerHWIDHere" -- buyer discord name
+-- {
+
+-- for more than one guy its this format
+-- return {
+--   "ExampleBuyerHWIDHere", -- buyer discord name
+--   "OtherBuyerHWIDHere" -- other buyer discord name
+-- {
+
+return {
+  "33e845ef-8b55-455a-8198-ea8548631e0f" -- wizard cat
+}
